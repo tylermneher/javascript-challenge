bbe1402bd27e03c692bcf42cf238d9837591af1a
Tyler M. Neher <tyler.neher@gmail.com>
2020-05-04 19:48:46 -0400

    second

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,5 +1,35 @@
 // from data.js
 var tableData = data;
+// Select tbody and make variable.
+var tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
-//
+// Using the UFO dataset provided in the form of an array of JavaScript objects, write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
+data.forEach(function (UFOreport) {
+  console.log(UFOreport);
+  var row = tbody.append("tr");
+
+  Object.entries(UFOreport).forEach(function ([key, value]) {
+    console.log(key, value);
+    // decl variable cell that appends another row to the html table
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+
+
+// Select the button
+var button = d3.select("#filter-btn");
+// what happens when the button is clicked
+button.on("click", function() {
+    // get the input element
+    var inputElement = d3.select("#datetime");
+    console.log(inputElement);
+    // Get the value property of the input element
+    var inputElementValue = inputElement.property("value");
+    console.log(inputElementValue);
+    // filter data 
+    var inputFilter = data.filter((obj) =>{
+        return obj.datetime == inputElementValue;
+    });
+    console.log(inputFilter);
+});

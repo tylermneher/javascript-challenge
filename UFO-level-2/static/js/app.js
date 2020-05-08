c2914fbdb6b248c86b8fe66597ddb9a16b4bcaf4
Tyler M. Neher <tyler.neher@gmail.com>
2020-05-08 16:41:43 -0400

    partial update to multifilter

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -7,11 +7,11 @@ var tbody = d3.select("tbody");
 
 // Using the UFO dataset provided in the form of an array of JavaScript objects, write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
 data.forEach(function (UFOreport) {
-  console.log(UFOreport);
+  // console.log(UFOreport);
   var row = tbody.append("tr");
 
   Object.entries(UFOreport).forEach(function ([key, value]) {
-    console.log(key, value);
+    // console.log(key, value);
     // decl variable cell that appends another row to the html table
     var cell = row.append("td");
     cell.text(value);
@@ -48,7 +48,7 @@ button.on("click", function () {
 
   // filter data
   var inputFilter = data.filter((obj) => {
-    return obj.datetime == inputElementValue;
+    return obj.datetime == inputElementValue_datetime;
   });
   console.log(inputFilter);
 });
